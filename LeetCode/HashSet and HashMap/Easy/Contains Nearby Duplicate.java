592865e7f923dddb673f9f69ceaf4e1dea4bb8eb
Ali Taladar <taladarali@gmail.com>
2021-06-14 14:27:08 -0400

    Create Contains Nearby Duplicate.java

---
+++ LeetCode/HashSet and HashMap/Easy/Contains Nearby Duplicate.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+        Map<Integer, Integer> myMap = new HashMap<>();
+        
+        for(int i = 0; i < nums.length; i++){
+            if(myMap.containsKey(nums[i]) && i - myMap.get(nums[i]) <= k){
+                return true;
+            }else
+                myMap.put(nums[i], i);
+        }
+        
+        return false;
+    }
+}
