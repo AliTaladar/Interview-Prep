ec814ef0188cb05192d59909522d87a42b16bd3e
Ali Taladar <taladarali@gmail.com>
2021-06-14 14:17:47 -0400

    Create Two Sum.java

---
+++ LeetCode/Array/Easy/Two Sum.java
@@ -0,0 +1,17 @@
+class Solution {
+   public int[] twoSum(int[] nums, int target) {
+    Map<Integer, Integer> myMap = new HashMap<>();
+
+        for(int i = 0; i < nums.length; i++){ // log n
+            int complement = target - nums[i];
+
+            if(myMap.containsKey(complement)){
+                return new int[] {i, myMap.get(complement)};
+            }
+            myMap.put(nums[i], i);
+            
+        }
+
+        return new int[] {};
+}
+}
