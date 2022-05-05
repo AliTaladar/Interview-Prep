9950583a14218a0f8d9f66bd7eb7aa8d026fedae
Ali Taladar <taladarali@gmail.com>
2022-05-05 19:06:50 -0400

    Update Insertion Sort.java

--- AlgoExpert/Sorting/Easy/Insertion Sort.java
+++ AlgoExpert/Sorting/Easy/Insertion Sort.java
@@ -1,19 +1,23 @@
 import java.util.*;
 
 class Program {
-  public static int[] insertionSort(int[] array) {
-    // Write your code here.
-		
-		for(int i = 0; i < array.length; i++){
-			for(int j = i; j > 0; j--){
-				if(array[j] < array[j - 1]){
-					int temp = array[j];
-					array[j] = array[j - 1];
-					array[j - 1] = temp;
-				}else break;
+	public static int[] insertionSort(int[] array) {
+		// Write your code here.
+
+		for (int i = 0; i < array.length; i++) {
+			int j = i;
+			while (j > 0 && array[j] < array[j - 1]) {
+				swap(j, j - 1, array);
+				j--;
 			}
 		}
-		
-    return array;
-  }
+
+		return array;
+	}
+
+	public static void swap(int i, int j, int[] array) {
+		int temp = array[i];
+		array[i] = array[j];
+		array[j] = temp;
+	}
 }
