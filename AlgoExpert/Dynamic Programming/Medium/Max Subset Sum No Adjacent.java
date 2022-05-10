dd688b3914cdbc7b6ad1307e7c32fefdad18e3dc
Ali Taladar <taladarali@gmail.com>
2022-05-10 17:55:37 -0400

    Update Max Subset Sum No Adjacent.java

--- AlgoExpert/Dynamic Programming/Medium/Max Subset Sum No Adjacent.java
+++ AlgoExpert/Dynamic Programming/Medium/Max Subset Sum No Adjacent.java
@@ -1,22 +1,20 @@
 import java.util.*;
 
 class Program {
-  public static int maxSubsetSumNoAdjacent(int[] array) {
-    // Write your code here.
-		if(array.length == 0) return 0;
-		else if(array.length == 1) return array[0];
-		
-		array[1] = Math.max(array[0], array[1]);
-		
-		for(int i = 2; i < array.length; i++){
-			
-			if(array[i - 2] + array[i] > array[i - 1]){
-				array[i] = array[i - 2] + array[i];
-			}else{
-				array[i] = array[i - 1];
-			}
+	public static int maxSubsetSumNoAdjacent(int[] array) {
+		// Write your code here.
+		if (array.length == 0) return 0;
+		if (array.length == 1) return array[0];
+
+		int first = array[0];
+		int second = Math.max(array[0], array[1]);
+
+		for (int i = 2; i < array.length; i++) {
+			int current = Math.max(array[i] + first, second);
+			first = second;
+			second = current;
 		}
-		
-    return array[array.length - 1];
-  }
+
+		return second;
+	}
 }
