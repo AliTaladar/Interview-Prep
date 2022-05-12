aff9f58e21f13d5588fb94437484c6f7bb8e505c
Ali Taladar <taladarali@gmail.com>
2022-05-12 11:16:06 -0400

    Update Number of Ways to Traverse Graph.java

--- AlgoExpert/Dynamic Programming/Medium/Number of Ways to Traverse Graph.java
+++ AlgoExpert/Dynamic Programming/Medium/Number of Ways to Traverse Graph.java
@@ -2,21 +2,22 @@ import java.util.*;
 
 class Program {
 
-  public int numberOfWaysToTraverseGraph(int width, int height) {
-    // Write your code here.
+	public int numberOfWaysToTraverseGraph(int width, int height) {
+		// Write your code here.
 		int[][] grid = new int[height][width];
-		grid[0][0] = 1;
-		
-		for(int i = 0; i < height; i++){
-			for(int j = 0; j < width; j++){
-				if(i == 0 && j == 0) continue;
-				int up = 0, left = 0;
-				if(i - 1 >= 0) up = grid[i - 1][j];
-				if(j - 1 >= 0) left = grid[i][j - 1];
-				grid[i][j] = up + left;
+
+		for (int i = 0; i < height; i++) {
+			for (int j = 0; j < width; j++) {
+				if (i == 0 || j == 0) {
+					grid[i][j] = 1;
+					continue;
+				}
+				int above = grid[i - 1][j];
+				int left = grid[i][j - 1];
+				grid[i][j] = above + left;
 			}
 		}
-		
-    return grid[height - 1][width - 1];
-  }
+
+		return grid[height - 1][width - 1];
+	}
 }
