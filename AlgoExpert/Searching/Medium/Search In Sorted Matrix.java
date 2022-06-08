051105774cccdefb2980e0bf440ed6b5c14e041d
Ali Taladar <taladarali@gmail.com>
2022-06-08 10:22:24 -0400

    Create Search In Sorted Matrix.java

---
+++ AlgoExpert/Searching/Medium/Search In Sorted Matrix.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+class Program {
+  public static int[] searchInSortedMatrix(int[][] matrix, int target) {
+    // Write your code here.
+    int row = 0;
+    int col = matrix[0].length - 1;
+
+    while (row < matrix.length && col >= 0) {
+      if (target > matrix[row][col]) {
+        row++;
+      } else if (target < matrix[row][col]) {
+        col--;
+      } else {
+        return new int[] {row, col};
+      }
+    }
+    
+    return new int[] {-1, -1};
+  }
+}
